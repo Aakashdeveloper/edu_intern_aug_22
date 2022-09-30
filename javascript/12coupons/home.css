7049947a1ad5f51d04c8e35d061f37e6544ef681
aakashdeveloper <ahanda205@gmail.com>
2022-09-30 20:25:09 +0530

    lightdark

---
+++ javascript/12coupons/home.css
@@ -0,0 +1,16 @@
+#coupon{
+    height:200px;
+    width:400px;
+    background-color:gray;
+    z-index:100;
+    margin-left:40%;
+    visibility: hidden;
+    position: absolute;
+    margin-top: -50%;
+    
+}
+
+#main{
+    background-color:orange;
+    height:800px;
+}
